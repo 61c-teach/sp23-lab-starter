14c0b6b80258c660b22b17593db8a82c5525e2a3
Jero Wang <jerowang2011@gmail.com>
2023-04-05 05:22:48 -0700

    [lab07] release

---
+++ lab07/ex2.h
@@ -0,0 +1,19 @@
+
+#ifndef EX2_H
+#define EX2_H
+
+#include <math.h>
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define REPEAT 60
+#define BUF_SIZE 8192
+
+#define ARRAY_SIZE 5000000
+
+void v_add_naive(double* x, double* y, double* z);
+void v_add_optimized_adjacent(double* x, double* y, double* z);
+void v_add_optimized_chunks(double* x, double* y, double* z);
+#endif
